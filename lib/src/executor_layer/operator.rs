6ebbeaa6dd690127927e31d514dfa9f927971262
fedorbykov <s20b1_bykov@179.ru>
2025-05-05 10:51:30 +0300

    Projection

--- lib/src/executor_layer/operator.rs
+++ lib/src/executor_layer/operator.rs
@@ -1,4 +1,3 @@
-use std::boxed::Box;
 use std::iter::Iterator;
 use std::ops::DerefMut;
 
@@ -31,5 +30,46 @@ impl PhysicalOperator for TableScan<'_> {
     fn open(&mut self) -> Result<(), ExecutorError> {
         self.iterator = Some(self.table_manager.iter(self.table_name.clone())?);
         Ok(())
+    }
+}
+
+pub struct Projection {
+    //Which columns to leave
+    columns: Vec<bool>,
+    source: Box<dyn PhysicalOperator>,
+}
+
+impl Projection {
+    pub fn project(&self, message: Message) -> Message {
+        Message {
+            fields: message
+                .fields
+                .into_iter()
+                .zip(self.columns.iter())
+                .filter_map(|(field, status)| {
+                    if *status {
+                        return Some(field);
+                    }
+                    None
+                })
+                .collect(),
+        }
+    }
+}
+
+impl Iterator for Projection {
+    type Item = Message;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.source
+            .deref_mut()
+            .next()
+            .map(|item| self.project(item))
+    }
+}
+
+impl PhysicalOperator for Projection {
+    fn open(&mut self) -> Result<(), ExecutorError> {
+        self.source.deref_mut().open()
     }
 }
