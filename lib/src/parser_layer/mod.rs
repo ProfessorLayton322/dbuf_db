c8f49395560283f681420c5d51debf40357ec847
fedorbykov <s20b1_bykov@179.ru>
2025-05-24 23:19:09 +0300

    Parsing tests

--- lib/src/parser_layer/mod.rs
+++ lib/src/parser_layer/mod.rs
@@ -9,19 +9,72 @@ mod tests {
     use super::*;
 
     #[test]
-    fn placeholder_test() {
+    fn query_parser_test() {
         let parser = query::QueryParser::new();
-        let input = r#"
+
+        let select = r#"
             SELECT user.age + 5 AS age_plus_five, user.name AS username
             FROM users
-            WHERE MATCH user.role { UserRole::Admin => user.level > 5 }
+            WHERE MATCH user.role { UserRole::Admin => user.level > 5 };
         "#;
 
-        match parser.parse(input) {
-            Ok(ast) => println!("{:#?}", ast),
-            Err(e) => eprintln!("Parse error: {:?}", e),
+        match parser.parse(select) {
+            Ok(ast::Query::Select {
+                table: _,
+                fields: _,
+                condition: _,
+            }) => {}
+            _ => panic!("Cant parse select query"),
         }
 
-        assert_eq!(2 + 2, 5);
+        let fetch = r#"
+            FETCH TYPES "something.dbuf";
+        "#;
+
+        match parser.parse(fetch) {
+            Ok(ast::Query::FetchTypes(_)) => {}
+            Err(e) => println!("{:?}", e),
+            _ => panic!("Cant parse fetch query"),
+        }
+
+        let create = r#"
+            CREATE TABLE user_table User;
+        "#;
+
+        match parser.parse(create) {
+            Ok(ast::Query::CreateTable(_, _)) => {}
+            _ => panic!("Cant parse create query"),
+        }
+
+        let drop = r#"
+            DROP TABLE user_table;
+        "#;
+
+        match parser.parse(drop) {
+            Ok(ast::Query::DropTable(_)) => {}
+            _ => panic!("Cant parse drop query"),
+        }
+
+        let insert = r#"
+            INSERT INTO user_table VALUES [User {"John", "Doe"}],  [User {"Jane", "Doe"}];
+        "#;
+
+        match parser.parse(insert) {
+            Ok(ast::Query::InsertMessages {
+                table: _,
+                messages: _,
+            }) => {}
+            _ => panic!("Cant parse insert query"),
+        }
+    }
+
+    use dbuf_core::parser::parse;
+
+    #[test]
+    fn fetch_types_test() {
+        let file = std::fs::read_to_string("sample_dbuf/user.dbuf").unwrap();
+        let parsed = parse(&file).unwrap();
+        println!("{:#?}", parsed);
+        //assert_eq!(2 + 2, 5);
     }
 }
